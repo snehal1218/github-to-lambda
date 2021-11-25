7faad7aa7a4f843962eb1b7f0e8c1685111379d5
Snehal Ramchandra Shendage <snehal.shendage@yash.com>
2021-11-25 20:54:49 +0530

    Lamnda changed

--- lambda_function.py
+++ lambda_function.py
@@ -2,6 +2,6 @@ import pandas as pd
 
 def lambda_handler(event, context):
    d = {'col1': [1,2],'col2': [3,4]}
-   df = pd.DataFrame(date=d)
+   df = pd.DataFrame(data=d)
    print(df)
    print('Done x1.1')
